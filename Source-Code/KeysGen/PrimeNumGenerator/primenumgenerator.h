42a774f8addea8d90094f6b57ee8e8ab6e7353ed
Elisha Lai <elisha.lai@outlook.com>
2015-02-16 02:19:17 -0500

    Create primenumgenerator.h
    Added primenumgenerator.h to current directory

---
+++ Source-Code/KeysGen/PrimeNumGenerator/primenumgenerator.h
@@ -0,0 +1,32 @@
+//====================================================================
+// Private & Public Keys Generator
+//
+// @author: Mark Luo & Elisha Lai
+// @description: Module for providing functions to work with
+// PrimeNumGenerator objects
+// @version: 1.2 30/01/2014
+//====================================================================
+
+// PrimeNumGenerator module (primenumgenerator.h)
+
+#ifndef __PRIMENUMGENERATOR_H__
+#define __PRIMENUMGENERATOR_H__
+
+// Object definition
+class PrimeNumGenerator {
+   long long primeNum;            // Randomly generated prime number
+
+   // Generates primeNum randomly.
+   void generatePrimeNum();
+
+public:
+   PrimeNumGenerator();     // Constructor
+   ~PrimeNumGenerator();    // Destructor
+
+   long long getPrimeNum() const; // Accessor to primeNum
+
+   // Regenerates primeNum randomly.
+   void regeneratePrimeNum();
+};
+
+#endif
